948d2aae9c612f6a7977619e4946a235ab2e133e
= <ladumer@hotmail.com>
2015-09-03 20:49:54 -0300

    Min-Max Implementation
    Running fine, but I need to work in the utility function

--- players/player_minimax.py
+++ players/player_minimax.py
@@ -40,5 +40,49 @@ class MinimaxPlayer(Player):
         #   It prints a board filled with the executed moves.
         #   WARNING: printing can slow your code. Use it just for debug.
         #
+        maxAction = None
+        maxValue = float('inf') * (-1)
+        available = find_empty_cells(board);
+        print available
+        for action in available:
+            res = self.result((board,0,1),action)
+            minVal = self.minValue(res)
+            if minVal > maxValue:
+                maxValue = minVal
+                maxAction = action
+        print 'moving to ',maxAction
+        return maxAction
 
-        return None
+    def result(self,state,action):
+        nBoard = list(state[0])
+        nBoard[action] = self.symbol
+        return (nBoard,find_empty_cells(nBoard),state[2]+1)
+
+    def minValue(self,state):
+        if self.isTerminal(state):
+            return self.utility(state)
+        v = float('inf')
+        for action in state[1]:
+            v = min(v,self.maxValue(self.result(state,action)))
+        return v
+
+    def maxValue(self,state):
+        if self.isTerminal(state):
+            return self.utility(state)
+        v = float('inf') * (-1)
+        for action in state[1]:
+            v = max(v,self.minValue(self.result(state,action)))
+        return v
+
+    def isTerminal(self,state):
+        return (len(state[1]) == 0) or (find_winner(state[0])[0] != None)
+
+    def utility(self,state):
+        winner = find_winner(state[0])[0]
+        free = state[1]
+        if winner == None:
+            return 5 * free * state[2]
+        if winner == self.symbol:
+            return 10 * free * state[2]
+
+        return -10 * free * state[2]
